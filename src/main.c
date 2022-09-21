8c03de03684596e102a8795ccea1bfc5304aa118
German Atalyants Arzumanov <germanchik01atalyants@gmail.com>
2022-09-22 00:57:45 +0200

    Small fixes

--- src/main.c
+++ src/main.c
@@ -10,6 +10,9 @@
 #include <string.h>
 
 void* handle_url_request(OAuth* oauth, const char* endpoint, const char* query) {
+
+    if (!oauth) return;
+
     const char* save = NULL;
     const char* token;
     query = str_create(query);
@@ -23,17 +26,16 @@ void* handle_url_request(OAuth* oauth, const char* endpoint, const char* query) 
 }
 
 int main(int argc, char* argv[]) {
-    srand(time_ms());
     OAuth* oauth = oauth_create("./TEST.ini");
     scheme_handler* handler = app_open(argc, argv, NULL, "TEST", handle_url_request, oauth);
     if (handler) {
+        srand(time_ms());
+
         oauth_append_data(oauth, "fields", "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics");
+
+        oauth_set_options(oauth, REQUEST_ASYNC | REQUEST_CACHE | REQUEST_AUTH);
 
         response_data* response = oauth_request(oauth, GET, "https://api.myanimelist.net/v2/anime/30230");
-
-        oauth_append_data(oauth, "fields", "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics");
-
-        response = oauth_request(oauth, GET, "https://api.myanimelist.net/v2/anime/30230");
 
         getchar();
         oauth_delete(oauth);
