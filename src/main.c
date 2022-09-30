ac0d89e43e0ddb2ef57f34fdfa93c7a2b223861f
German Atalyants Arzumanov <germanchik01atalyants@gmail.com>
2022-09-30 13:34:29 +0200

    Where is this

--- src/main.c
+++ src/main.c
@@ -32,7 +32,7 @@ int main(int argc, char* argv[]) {
 
         oauth_append_data(oauth, "fields", "id,title,main_picture,alternative_titles,start_date,end_date,synopsis,mean,rank,popularity,num_list_users,num_scoring_users,nsfw,created_at,updated_at,media_type,status,genres,my_list_status,num_episodes,start_season,broadcast,source,average_episode_duration,rating,pictures,background,related_anime,related_manga,recommendations,studios,statistics");
 
-        oauth_set_options(oauth, REQUEST_CACHE | REQUEST_AUTH);
+        oauth_set_options(oauth, REQUEST_ASYNC | REQUEST_CACHE | REQUEST_AUTH);
 
         response_data* response = oauth_request(oauth, GET, "https://api.myanimelist.net/v2/anime/30230");
 
